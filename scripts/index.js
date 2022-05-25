d31042c38b3798bf06763990dd181b97a3241315
softlolx <softlolx@gmail.com>
2022-05-25 21:52:27 +0300

    feature: built new popups

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,20 @@
-let profileEditPopup = document.querySelector(".popup_content_profile");
-let profileEditForm = document.querySelector(".popup__form");
-let profileName = document.querySelector(".profile__name");
-let profileDescription = document.querySelector(".profile__description");
-let editProfileButton = document.querySelector(".profile__edit-button");
-let closePopupButton = document.querySelector(".popup__close-button");
-let nameInput = document.querySelector(".popup__input_type_name");
-let descriptionInput = document.querySelector(".popup__input_type_description");
+const profileEditPopup = document.querySelector(".popup_content_profile");
+const itemAddPopup = document.querySelector(".popup_content_add-item");
+const profileEditForm = document.querySelector(".popup__form_content_profile");
+const itemAddForm = document.querySelector(".popup__form_content_add-item");
+const profileName = document.querySelector(".profile__name");
+const profileDescription = document.querySelector(".profile__description");
+const editProfileButton = document.querySelector(".profile__edit-button");
+const addItemButton = document.querySelector(".profile__add-button");
+const closePopupButton = document.querySelector(".popup__close-button");
+const nameInput = document.querySelector(".popup__input_type_name");
+const descriptionInput = document.querySelector(
+  ".popup__input_type_description"
+);
 
-function openPopup() {
-  profileEditPopup.classList.add("popup_opened");
-  profileEditForm.reset();
+function openPopup(popup, form) {
+  popup.classList.add("popup_opened");
+  form.reset();
   nameInput.setAttribute("value", profileName.innerText);
   descriptionInput.setAttribute("value", profileDescription.innerText);
 }
@@ -25,6 +30,11 @@ function formSubmitHandler(evt) {
   closePopup();
 }
 
-editProfileButton.addEventListener("click", openPopup);
+editProfileButton.addEventListener("click", () => {
+  openPopup(profileEditPopup, profileEditForm);
+});
+addItemButton.addEventListener("click", () => {
+  openPopup(itemAddPopup);
+});
 closePopupButton.addEventListener("click", closePopup);
 profileEditForm.addEventListener("submit", formSubmitHandler);
